602c94be2a37db9721d3dc3dfb384f088d09cbfc
Ben Keith <benkeith@splunk.com>
2021-10-04 13:58:20 -0400

    Add gRPC interceptor to request tracker (#224)
    This makes those metrics reusable between HTTP and gRPC servers

--- web/reqcounter_test.go
+++ web/reqcounter_test.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"context"
 	"net/http"
 	"testing"
 	"time"
@@ -26,4 +27,14 @@ func TestRequestCounter(t *testing.T) {
 	m.ServeHTTP(nil, nil, f)
 
 	assert.Equal(t, 3, len(m.Datapoints()))
+
+	resp, err := m.GRPCInterceptor(context.Background(), nil, nil, func(ctx context.Context, req interface{}) (interface{}, error) {
+		assert.EqualValues(t, 1, m.ActiveConnections)
+		assert.EqualValues(t, 3, m.TotalConnections)
+		assert.NotEqual(t, 0, m.TotalProcessingTimeNs)
+
+		return 5, nil
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, 5, resp)
 }
